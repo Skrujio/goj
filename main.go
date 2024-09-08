a5ed90c57974fb0d408ce1f8a726fae71ad4e6cf
Skrujio <37263391+Skrujio@users.noreply.github.com>
2024-09-09 04:26:09 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -15,26 +14,28 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+var key = []byte("secret")
+
 func main() {
 	dbUser := "postgres"  // os.Getenv("POSTGRES_USER")
 	dbPassword := "admin" // os.Getenv("POSTGRES_PASSWORD")
 	dbName := "postgres"  // os.Getenv("POSTGRES_NAME")
+
 	db, err := sql.Open("postgres",
 		fmt.Sprintf("postgres://%s:%s@localhost:5432/%s?sslmode=disable",
 			dbUser, dbPassword, dbName))
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	defer db.Close()
 
 	creatTable(db)
-
 	server := DBServer{db}
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /tokens/{guid}", server.tokensHandler)
-	mux.HandleFunc("POST /refresh", server.refreshHandler)
+	mux.HandleFunc("GET /refresh", server.refreshHandler)
 
 	http.ListenAndServe(":8080", mux)
 }
@@ -55,176 +56,170 @@ type DBServer struct {
 	db *sql.DB
 }
 
-type Tokens struct {
-	RefreshBase64 string
-	Access        string
+func (s DBServer) tokensHandler(w http.ResponseWriter, r *http.Request) {
+	ip := getIP(r)
+	guid := r.PathValue("guid")
+
+	refreshTokenString, err := createRefreshTokenString(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	accessTokenString, err := createAccessTokenString(guid, ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	refreshHash, err := getRefreshHash(refreshTokenString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.db.Exec(`INSERT INTO app_table (guid, ip, refreshHash) VALUES ($1, $2, $3) ON CONFLICT (guid) DO UPDATE SET ip=$2, refreshHash=$3`, guid, ip, refreshHash)
+
+	setTokenCookies(w, refreshTokenString, accessTokenString)
+	// ref, acc := getTokensFromCookies(w, r)
+	// fmt.Println(ref, acc)
 }
 
-func (s DBServer) tokensHandler(w http.ResponseWriter, req *http.Request) {
-	reqGUID := getGUID(req)
-	reqIP := getIP(req)
+func getRefreshHash(tokenString string) ([]byte, error) {
+	s := getTokenSignature(tokenString)
+	return HashPassword(s)
+}
 
-	var dbIP string
-	row := s.db.QueryRow(`SELECT ip FROM app_table WHERE guid=$1`, reqGUID)
+func (s DBServer) refreshHandler(w http.ResponseWriter, r *http.Request) {
+	refreshTokenString, accessTokenString := getTokensFromCookies(w, r)
 
-	refreshTokenSignedString, err := createRefreshTokenSignedString(reqGUID, reqIP)
+	refreshToken, err := parseTokenString(refreshTokenString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	accessToken, err := parseTokenString(accessTokenString)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// refreshTokenHash, err := HashPassword(refreshTokenSignedString)
+	claimsIP := refreshToken.Claims.(jwt.MapClaims)["ip"].(string)
+	claimsGUID := accessToken.Claims.(jwt.MapClaims)["guid"].(string)
+
+	newRefreshTokenString, err := createRefreshTokenString(claimsIP)
 	if err != nil {
-		fmt.Print(len(refreshTokenSignedString))
+		log.Fatal(err)
+	}
+	newAccessTokenString, err := createAccessTokenString(claimsGUID, claimsIP)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	switch err := row.Scan(&dbIP); err {
-	case sql.ErrNoRows:
-		fmt.Println("ErrNoRows")
-		_, err := s.db.Exec(`INSERT INTO app_table (guid, ip, refreshHash) VALUES ($1, $2, $3)`, reqGUID, reqIP, refreshTokenSignedString)
-		fmt.Println(err)
+	reqIP := getIP(r)
+	if claimsIP != reqIP {
+		sendNotification()
+	}
 
-	case nil:
-		fmt.Println("nil")
-		if reqIP != dbIP {
-			s.db.Exec(`UPDATE app_table ip=$1 WHERE guid=$2`, reqIP, reqGUID)
+	row := s.db.QueryRow(`SELECT refreshHash FROM app_table WHERE ip=$1`, claimsIP)
+
+	var refreshHash []byte
+	err = row.Scan(&refreshHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = bcrypt.CompareHashAndPassword(refreshHash, []byte(getTokenSignature(string(refreshTokenString))))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newRefreshHash, err := getRefreshHash(newRefreshTokenString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = s.db.Exec(`UPDATE app_table SET ip=$1, refreshHash=$2 WHERE ip=$3`, reqIP, newRefreshHash, claimsIP)
+	if err != nil {
+		fmt.Println("asd")
+		log.Fatal(err)
+	}
+
+	setTokenCookies(w, newRefreshTokenString, newAccessTokenString)
+}
+
+func getTokenSignature(TokenString string) string {
+	arr := strings.Split(TokenString, ".")
+	return arr[2]
+}
+
+func sendNotification() {
+
+}
+
+func setTokenCookies(w http.ResponseWriter, refreshToken, accessToken string) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "refreshToken",
+		Value: refreshToken,
+		Path:  "/",
+	})
+	http.SetCookie(w, &http.Cookie{
+		Name:  "accessToken",
+		Value: accessToken,
+		Path:  "/",
+	})
+}
+
+func getTokensFromCookies(w http.ResponseWriter, r *http.Request) (string, string) {
+	refreshTokenString := getCookieValue(w, r, "refreshToken")
+	accessTokenString := getCookieValue(w, r, "accessToken")
+
+	return refreshTokenString, accessTokenString
+}
+
+func getCookieValue(w http.ResponseWriter, r *http.Request, cookieName string) string {
+	c, err := r.Cookie(cookieName)
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
 		}
-
-		s.db.Exec(`UPDATE app_table refreshHash=$1 WHERE guid=$2`, refreshTokenSignedString, reqGUID)
-	default:
+		w.WriteHeader(http.StatusBadRequest)
 		log.Fatal(err)
 	}
-
-	refreshTokenBase64 := base64.StdEncoding.EncodeToString([]byte(refreshTokenSignedString))
-	accessTokenSignedString, err := createAccessTokenSignedString(reqGUID, reqIP, base64.StdEncoding.EncodeToString([]byte(refreshTokenSignedString)))
-	parseTokenString(accessTokenSignedString)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = json.NewEncoder(w).Encode(Tokens{
-		refreshTokenBase64,
-		accessTokenSignedString,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	return c.Value
 }
 
-func (s DBServer) refreshHandler(w http.ResponseWriter, req *http.Request) {
-	var tokens Tokens
-	err := json.NewDecoder(req.Body).Decode(&tokens)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	refreshTokenSignedString, err := base64.StdEncoding.DecodeString(tokens.RefreshBase64)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	refreshTokenClaims := parseTokenString(string(refreshTokenSignedString)).Claims.(jwt.MapClaims)
-
-	// if int64(refreshTokenClaims["exp"].(float64)) < time.Now().Unix() {
-	// 	log.Fatal(fmt.Errorf("refresh token is expired"))
-	// }
-
-	accessTokenClaims := parseTokenString(tokens.Access).Claims.(jwt.MapClaims)
-
-	// if int64(accessTokenClaims["exp"].(float64)) < time.Now().Unix() {
-	// 	log.Fatal(fmt.Errorf("access token is expired"))
-	// }
-
-	if refreshTokenClaims["guid"] != accessTokenClaims["guid"] || tokens.RefreshBase64 != accessTokenClaims["refreshTokenBase64"].(string) {
-		log.Fatal(fmt.Errorf("invalid pair of tokens"))
-	}
-
-	reqGUID := refreshTokenClaims["guid"].(string)
-	reqIP := getIP(req)
-
-	var dbIP string
-	var dbRefreshHash string
-	err = s.db.QueryRow(`SELECT ip, refreshHash FROM app_table WHERE guid=$1`, reqGUID).Scan(&dbIP, &dbRefreshHash)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if reqIP != dbIP {
-		s.db.Exec(`UPDATE app_table ip=$1 WHERE guid=$2`, reqIP, reqGUID)
-	}
-
-	// err = bcrypt.CompareHashAndPassword([]byte(dbRefreshHash), refreshTokenSignedString)
-	// if []byte(dbRefreshHash) != refreshTokenSignedString { // err != nil {
-	// 	fmt.Println(dbRefreshHash)
-	// 	// fmt.Println(refreshTokenSignedString)
-	// 	fmt.Println("3")
-	// 	log.Fatal(fmt.Errorf("mismatching refresh token"))
-	// }
-
-	refreshTokenBase64 := base64.StdEncoding.EncodeToString([]byte(refreshTokenSignedString))
-	accessTokenSignedString, err := createAccessTokenSignedString(reqGUID, reqIP, base64.StdEncoding.EncodeToString([]byte(refreshTokenSignedString)))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(refreshTokenBase64)
-	fmt.Println(accessTokenSignedString)
-
-	err = json.NewEncoder(w).Encode(Tokens{
-		refreshTokenBase64,
-		accessTokenSignedString,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getGUID(r *http.Request) string {
-	return r.PathValue("guid")
-}
-
-func getIP(r *http.Request) string {
-	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
-	return ip
-}
-
-func createRefreshTokenSignedString(id, ip string) (string, error) {
+func createRefreshTokenString(ip string) (string, error) {
 	refreshTokenExpirationTime := time.Now().Add(5 * time.Minute).Unix()
-	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-		"guid": id,
-		"ip":   ip,
-		"iat":  refreshTokenExpirationTime,
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		// "guid": id,
+		"ip":  ip,
+		"exp": refreshTokenExpirationTime,
 	})
 	return refreshToken.SignedString(key)
 }
 
-func createAccessTokenSignedString(id, ip, refreshTokenBase64 string) (string, error) {
+func createAccessTokenString(guid, ip string) (string, error) {
 	accessTokenExpirationTime := time.Now().Add(2 * time.Minute).Unix()
 	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-		"guid":               id,
-		"ip":                 ip,
-		"refreshTokenBase64": refreshTokenBase64,
-		"iat":                accessTokenExpirationTime,
+		"guid": guid,
+		"ip":   ip,
+		// "refreshTokenBase64": refreshTokenBase64,
+		"exp": accessTokenExpirationTime,
 	})
 	return accessToken.SignedString(key)
 }
 
-func parseTokenString(tokenString string) *jwt.Token {
+func HashPassword(password string) ([]byte, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return hashedPassword, err
+}
+
+func parseTokenString(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(string(tokenString), func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return key, nil
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	return token
+	return token, err
 }
 
-var key = []byte("secret")
-
-func HashPassword(password string) ([]byte, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return hashedPassword, err
+func getIP(r *http.Request) string {
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	return ip
 }
